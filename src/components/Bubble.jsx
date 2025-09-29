e12b81be5d1f215068c9eae8065c54912381cba3
Franco Barberis <francobarberissic3@gmail.com>
2025-09-29 09:43:27 -0300

    cursor-pointer on bubbles

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -1,7 +1,7 @@
 
 function Bubble({icon, tooltip}) {
   return (
-    <div className="w-16 h-16 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg ">
+    <div className="w-16 h-16 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg hover:cursor-pointer ">
       <span className="text-white text-2xl font-bold">{icon}</span>
     </div>
   );
