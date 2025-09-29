48e465003121498b1d6051fbde267a2cb2f3825d
Franco Barberis <francobarberissic3@gmail.com>
2025-09-29 14:55:36 -0300

    Tooltips on bubbles, no animation

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -1,8 +1,13 @@
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 function Bubble({icon, tooltip}) {
   return (
-    <div className="w-16 h-16 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg hover:cursor-pointer ">
+    <div>
+      <div data-tooltip-id="bubble-tooltip" data-tooltip-content={tooltip} className="w-16 h-16 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg hover:cursor-pointer ">
       <span className="text-white text-2xl font-bold">{icon}</span>
+      </div>
+      <Tooltip id="bubble-tooltip" place="right" />
     </div>
   );
 }
