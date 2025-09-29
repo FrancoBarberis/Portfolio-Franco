b96f457ab194dabdd84da94536b04b6d13eb2b54
Franco Barberis <francobarberissic3@gmail.com>
2025-09-29 15:36:09 -0300

    Fixed animation on tooltips

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -1,30 +1,21 @@
-import { useState } from "react";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 
 function Bubble({ icon, tooltip, id }) {
-  const [active, setActive] = useState(false);
   return (
     <div
-      onMouseEnter={() => setActive(true)}
-      onMouseLeave={() => setActive(false)}
-      className="relative"
+      data-tooltip-id={id}
+      data-tooltip-content={tooltip}
+      className="relative w-auto h-auto p-3.5 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg hover:cursor-pointer"
     >
-      <div
-        data-tooltip-id="{id}"
-        data-tooltip-content={tooltip}
-        className="w-16 h-16 bg-blue-500 rounded-xl flex items-center justify-center shadow-lg hover:cursor-pointer "
-      >
-        <span className="text-white text-2xl font-bold">{icon}</span>
-      </div>
-      {active && (
-        <Tooltip
-          id="{id}"
-          place="right"
-          delayShow={150}
-          className="tooltip-slide"
-        />
-      )}
+      <span className="text-2xl text-white">{icon}</span>
+
+      <Tooltip
+        id={id}
+        place="right"
+        delayShow={50}
+        className="tooltip-slide"
+      />
     </div>
   );
 }
